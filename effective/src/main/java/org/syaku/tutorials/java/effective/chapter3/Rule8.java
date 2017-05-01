c3e846a0b2fd4e10b4c030c876258e1bf9cd0278
syaku <syaku@naver.com>
2017-05-01 14:23:57 +0900

    이팩티브

--- effective/src/main/java/org/syaku/tutorials/java/effective/chapter3/Rule8.java
+++ effective/src/main/java/org/syaku/tutorials/java/effective/chapter3/Rule8.java
@@ -1,13 +1,22 @@
 package org.syaku.tutorials.java.effective.chapter3;
 
 /**
- * equals 를 재정의할 때는 일반 규약을 따르라
+ * 규칙 8 equals 를 재정의할 때는 일반 규약을 따르라
+ *
  * @author Seok Kyun. Choi. 최석균 (Syaku)
  * @site http://syaku.tistory.com
  * @since 2017. 4. 25.
  */
 public class Rule8 {
 
-	// 1. 모든 객체는 오직 자기 자신하고만 같다. 아래의 조건 가운데 하나라도 만족하면 재정의하지 않아도 된다.
-	// 각각의 객체가 고유하다. active entity 란 무엇인가?
+	public static void main(String[] args) {
+		String name = "syaku";
+		String name2 = "syaku";
+		name2 = "3333";
+
+		System.out.println(name.hashCode());
+		System.out.println(name2.hashCode());
+
+		System.out.println(name.equals(name2));
+	}
 }
